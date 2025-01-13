eb89908000cba47bc8a09d9fb4a2e9b58dbe47ff
leggen-assis <souzaferreira437@gmail.com>
2025-01-13 13:24:34 -0300

    random coordinates added

--- implementation/src/main.cpp
+++ implementation/src/main.cpp
@@ -4,18 +4,21 @@
 #include <string>
 #include <vector>
 #include "Graph.hpp"
+#include <random>
 
 std::unordered_map<std::string, size_t> map_coordinates_to_vertices;
 std::unordered_map<size_t, std::string> map_vertices_to_coordinates;
 std::vector<std::vector<std::string>> cartesian_plane;
+constexpr size_t COORDINATES_AMOUNT = 31 * 31;
+
 
 void buildMatrix() {
     size_t vertex {0};
     cartesian_plane.resize(31, std::vector<std::string>(31));
     std::string coordenate {};
     
-    map_vertices_to_coordinates.reserve(961);
-    map_coordinates_to_vertices.reserve(961);
+    map_vertices_to_coordinates.reserve(COORDINATES_AMOUNT);
+    map_coordinates_to_vertices.reserve(COORDINATES_AMOUNT);
 
     for (size_t x {0}; x < 31; ++x) {
         for (size_t y {0}; y < 31; ++y) {
@@ -67,23 +70,42 @@ void buildGraphFile() {
 }
 
 int main(void) {
+
+	// instanciações necessárias para gerar o número pseudo-aleatório
+	
+	std::random_device random_number;
+	std::mt19937 seed(random_number());
+	std::uniform_int_distribution<size_t> gap(0, COORDINATES_AMOUNT - 1);
+	
+	std::string x_source {};
+	std::string y_source {};
+	std::string x_destination {};
+	std::string y_destination {};
+		
     buildGraphFile();
+    
     Graph graph("graph.txt", cartesian_plane, map_vertices_to_coordinates, map_coordinates_to_vertices, false);
     
+    x_source = graph.coordinatesToString(graph.vertexToCoordinate(gap(seed)));
+	y_source = graph.coordinatesToString(graph.vertexToCoordinate(gap(seed)));
+	
+	x_destination = graph.coordinatesToString(graph.vertexToCoordinate(gap(seed)));
+	y_destination = graph.coordinatesToString(graph.vertexToCoordinate(gap(seed)));
+    
     std::cout << "********* BFS ********* \n";
-    graph.breadthFirstSearch("(5,1)", "(0,0)", 3);
+    graph.breadthFirstSearch(x_source, y_source, 3);
     
     std::cout << "\n\n ********* DFS ********* \n";
-    graph.depthFirstSearch("(5,1)", "(0,0)", 3);
+    graph.depthFirstSearch(x_source, y_source, 3);
     
     std::cout << "\n\n ********* Uniform Search (Dijkstra) ********* \n";
-    graph.uniformCostSearch("(5,1)", "(0,0)", 3);
+    graph.uniformCostSearch(x_source, y_source, 3);
     
     std::cout << "\n\n ********* A* ********* \n";
-    graph.AStar("(5,1)", "(0,0)", 3);
+    graph.AStar(x_source, y_source, 3);
     
     std::cout << "\n\n ********* Greedy Search ********* \n";
-    graph.greedySearch("(5,1)", "(0,0)", 3);
+    graph.greedySearch(x_source, y_source, 3);
     return 0;
 }
 
