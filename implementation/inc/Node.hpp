0848ce82a2a59aab99ffea7b9b0e6df01baf067f
leggen-assis <souzaferreira437@gmail.com>
2025-01-17 15:55:47 -0300

    shared pointers logics added

--- implementation/inc/Node.hpp
+++ implementation/inc/Node.hpp
@@ -1,6 +1,8 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include <iostream>
+#include <memory>
 #include <functional>
 
 struct Node {
@@ -9,35 +11,36 @@ struct Node {
     size_t heuristic_cost;
     size_t path_cost;
     size_t height;
-    bool drugstore_visited;
+    std::shared_ptr<Node> parent; // Alterado para shared_ptr
 
     // Construtor genérico
-    Node(size_t vertex, size_t value = 0, size_t heuristic_cost = 0, size_t path_cost = 0, size_t height = 0, bool drugstore_visited = false) :
+    Node(size_t vertex, size_t value = 0, size_t heuristic_cost = 0, size_t path_cost = 0, std::shared_ptr<Node> parent = nullptr, size_t height = 0) :
          vertex(vertex), value(value), heuristic_cost(heuristic_cost),
-         path_cost(path_cost), height(height), drugstore_visited(drugstore_visited) {}
+         path_cost(path_cost), height(height), parent(parent) {}
 
     Node() = default;
     
+    // Destruidor padrão, sem necessidade de implementação manual para shared_ptr
     ~Node() = default;
 
     // Sobrecarga do operador =
     Node& operator=(const Node& other) {
         if (this == &other) {
-            return *this; 
+            return *this;
         }
 
         this->vertex = other.vertex;
         this->value = other.value;
         this->heuristic_cost = other.heuristic_cost;
         this->path_cost = other.path_cost;
+        this->parent = other.parent; // Copiando shared_ptr
         this->height = other.height;
-        this->drugstore_visited = other.drugstore_visited;
 
         return *this;
     }
 
     bool operator == (const Node& node) const {
-        return (vertex == node.vertex) && (value == node.value) && (drugstore_visited == node.drugstore_visited);
+        return (vertex == node.vertex) && (value == node.value);
     }
 
     bool operator < (const Node& node) const {
@@ -54,10 +57,9 @@ struct NodeHash {
     size_t operator()(const Node& node) const {
         size_t h1 = std::hash<size_t>{}(node.vertex);
         size_t h2 = std::hash<size_t>{}(node.value);
-        size_t h3 = std::hash<bool>{}(node.drugstore_visited);
 
-        // Combine os hashes de vertex e value (podemos usar XOR ou soma)
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
+        // Combine os hashes de vertex, value e drugstore_visited de forma mais robusta
+        return h1 ^ (h2 << 1);
     }
 };
 
